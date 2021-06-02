51ef361698eb2678c4c7471f28a2bcd178957081
Duwe Aynab <DuweAynab@icloud.com>
2021-06-02 05:38:22 -0400

    Testing Update
    Testing complete

---
+++ src/main/java/org/wecancodeit/courses/CourseRepository.java
@@ -0,0 +1,29 @@
+package org.wecancodeit.courses;
+
+
+import org.springframework.stereotype.Repository;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+@Repository
+public class CourseRepository {
+
+    Map<Long, Course> coursesList = new HashMap<>();
+
+    //Constructor for testing purposes only..use varArgs to add none or as many objects as we want.
+    public CourseRepository(Course ...coursesToAdd) {
+        for(Course course: coursesToAdd) {
+            coursesList.put(course.getId(), course);
+        }
+    }
+
+    public Course findOne(long id) {
+        return coursesList.get(id);
+    }
+
+    public Collection<Course> findAll() {
+        return coursesList.values();
+    }
+}
