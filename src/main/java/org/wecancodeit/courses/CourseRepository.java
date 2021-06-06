b05e05aa42a4356520cb5aba5d2e34247310e58f
Duwe Aynab <DuweAynab@icloud.com>
2021-06-06 10:52:45 -0400

    Update

--- src/main/java/org/wecancodeit/courses/CourseRepository.java
+++ src/main/java/org/wecancodeit/courses/CourseRepository.java
@@ -12,6 +12,21 @@ public class CourseRepository {
 
     Map<Long, Course> coursesList = new HashMap<>();
 
+    //constructor to handle our database
+    public CourseRepository(){
+        Course reading = new Course( 1l, "Reading", "I love J.R.R. Tolkien, learn all about about this author in this course.");
+        Course music = new Course( 2l, "Music", "Love music? Join us. In this course, we dive deep into music's history and it's many genres.");
+        Course art = new Course( 3l, "Art", "Anything can be art. In this course we learn about the art all around us.");
+        Course history = new Course( 4l, "History", "American-History: Post-Civil War Era. Reconstruction Era to present-day.");
+
+
+        coursesList.put(reading.getId(), reading);
+        coursesList.put(music.getId(), music);
+        coursesList.put(art.getId(), art);
+        coursesList.put(history.getId(), history);
+
+    }
+
     //Constructor for testing purposes only..use varArgs to add none or as many objects as we want.
     public CourseRepository(Course ...coursesToAdd) {
         for(Course course: coursesToAdd) {
